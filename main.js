6e9c778f344d67755d6194faab7e68378049f5c9
evgg0r <gor.evg2100@gmail.com>
2025-02-22 12:43:31 +0700

    updating the weather logic

--- main.js
+++ main.js
@@ -22,15 +22,11 @@ const addCity = () => {
                 const cityName = document.querySelector('.city-name');
                 cityName.textContent = data.name;
 
-                const temperature = document.querySelector('.temperature');
-                const tempValue = Math.floor(data.main.temp - 273.15);
-                temperature.textContent = tempValue;
 
                 if (inputText !== "") {
                     const newCity = {
                         id: new Date().getTime(),
                         nameCity: data.name,
-                        temper: tempValue,
                         statusFavourites: false,
                     };
 
@@ -80,20 +76,92 @@ favouritesBtn.addEventListener('click', () => {
 
     if (city) {
         if (!city.statusFavourites)
-        city.statusFavourites = !city.statusFavourites;
+            city.statusFavourites = !city.statusFavourites;
     }
     renderCities();
 });
 
 const list = document.querySelector('.city-list');
 list.addEventListener('click', (e) => {
-    if (e.target.tagName === 'B') { // Проверяем, что кликнули на элемент <b>
-        const noteId = +e.target.closest('li').id; 
-        const index = cities.findIndex(city => city.id === noteId); 
+    if (e.target.tagName === 'B') {
+        const noteId = +e.target.closest('li').id;
+        const index = cities.findIndex(city => city.id === noteId);
 
         if (index !== -1) {
-            cities.splice(index, 1); 
-            renderCities(); 
+            cities.splice(index, 1);
+            renderCities();
         }
     }
 });
+
+const cityName = document.querySelector('.city-name')
+cityName.addEventListener('click', addDataWeather)
+
+function addDataWeather() {
+    const citySpan = document.querySelector('.city-name');
+    const cityName = citySpan.textContent
+
+
+    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f'; // 
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Ошибка сети ${response.status}`)
+            }
+            return response.json()
+        })
+        .then(data => {
+            console.log('Данные:', data);
+
+            const tempValue = Math.floor(data.main.temp - 273.15);
+            const temperature = document.querySelector('.temperature');
+            temperature.textContent = tempValue;
+
+
+            const tempFeels = Math.floor(data.main.feels_like - 273.15);
+            const feelsLikeEl = document.querySelector('.feels-like');
+            const feelsLikeValue = feelsLikeEl.querySelector('.value');
+            feelsLikeValue.textContent = tempFeels
+
+
+            const timeSunrise = new Date (data.sys.sunrise * 1000)
+            const hoursSunrise = String(timeSunrise.getHours()).padStart(2, '0'); 
+            const minutesSunrise = String(timeSunrise.getMinutes()).padStart(2, '0');
+            const timeSunriseValue = `${hoursSunrise}:${minutesSunrise}`;
+
+            const sunriseEl = document.querySelector('.sunrise');
+            const sunriseValue = sunriseEl.querySelector('.value');
+            sunriseValue.textContent = timeSunriseValue
+
+
+            const timeSunset = new Date (data.sys.sunset * 1000)
+            const hoursSunset = String(timeSunset.getHours()).padStart(2, '0'); 
+            const minutesSunset = String(timeSunset.getMinutes()).padStart(2, '0');
+            const timeSunsetValue = `${hoursSunset}:${minutesSunset}`;
+
+            const sunsetEl = document.querySelector('.sunset');
+            const sunsetValue = sunsetEl.querySelector('.value');
+            sunsetValue.textContent = timeSunsetValue
+
+            const iconWeather = document.querySelectorAll('.weather-icon');
+            const cloudsIcon = data.weather[0].icon
+            console.log(cloudsIcon);
+            
+
+            if (cloudsIcon === '13d') {
+                iconWeather.forEach((icon) => {
+                    icon.src = './icon-rain.svg';
+                })
+            } else if (cloudsIcon === '02d' || cloudsIcon === '04d') {
+                iconWeather.forEach((icon) => {
+                    icon.src = './icon-cloud.svg';
+                })
+            }
+        })
+        .catch(error => {
+            console.error('Произошла ошибка:', error.message);
+        });
+}
